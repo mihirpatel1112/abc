ae6ac30273fe93483a8ffde22a849db633e601dc
TheMightyM <159679690+mihirpatel1112@users.noreply.github.com>
2024-04-21 22:12:56 +1000

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+// tailwind.config.js
+
+module.exports = {
+    theme: {
+      extend: {
+        fontWeight: {
+          'ultralight': 100,
+          'extralight': 200,
+          // Add or customize font weights as needed
+        },
+      },
+    },
+    variants: {},
+    plugins: [],
+  }
+  
