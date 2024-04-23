a84578e7164c77a9831e49dd7b00202625f05301
TheMightyM <159679690+mihirpatel1112@users.noreply.github.com>
2024-04-23 16:18:03 +1000

    Add files via upload

---
+++ sendmail.php
@@ -0,0 +1,36 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = strip_tags(trim($_POST["name"]));
+    $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
+    $message = trim($_POST["message"]);
+
+    // Set the recipient email address
+    $recipient = "mihirpatel.mp.1112@gmail.com";
+
+    // Set the email subject
+    $subject = "New contact from $name";
+
+    // Build the email content
+    $email_content = "Name: $name\n";
+    $email_content .= "Email: $email\n\n";
+    $email_content .= "Message:\n$message\n";
+
+    // Build the email headers
+    $email_headers = "From: $name <$email>";
+
+    // Send the email
+    if (mail($recipient, $subject, $email_content, $email_headers)) {
+        // Set a 200 (okay) response code
+        http_response_code(200);
+        echo "Thank You! Your message has been sent.";
+    } else {
+        // Set a 500 (internal server error) response code
+        http_response_code(500);
+        echo "Oops! Something went wrong, and we couldn't send your message.";
+    }
+} else {
+    // Not a POST request, set a 403 (forbidden) response code
+    http_response_code(403);
+    echo "There was a problem with your submission, please try again.";
+}
+?>
